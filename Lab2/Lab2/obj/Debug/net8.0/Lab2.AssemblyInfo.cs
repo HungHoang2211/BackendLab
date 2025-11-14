4a7b07b2cc69e967c50138454923b7bdf0ac557e
Hưng <hunghdqpk043359@gmail.com>
2025-11-14 14:31:13 +0700

    Lab2 Bài 4

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710e9de9a2521401d847168257dcd9fde394cda1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2129f1e31d47243b40d805e78ed2dbf219d5c70b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
