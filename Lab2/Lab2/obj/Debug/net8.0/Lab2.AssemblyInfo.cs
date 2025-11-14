e5e78acfae72d04d77f8d28faf8d7ededbc73ba1
Hưng <hunghdqpk043359@gmail.com>
2025-11-14 08:57:27 +0700

    Solution Lab2, code Lab1 chuẩn bị cho Lab2

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3d3f2f69bf8aeebad9935a760e249662b3da2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
